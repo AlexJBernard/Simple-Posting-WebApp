cb524e7bf49afcc2e768ce824c7edd1ff431c90e
alexjbernard <alexjbernard.nc@gmail.com>
2025-03-11 17:15:07 -0400

    UPDATE: Created UserService file using fetchAPI

--- vueapp/src/services/UserService.js
+++ vueapp/src/services/UserService.js
@@ -26,10 +26,74 @@ export default {
           if (answer.statusCode == 200) {
             return answer.data
           } else {
-            return answer.error.description
+            console.log(answer.error.description)
+            return null
           }
         })
         console.log(answer)
+        return answer
+      }
+    ).catch(
+      (error) => {
+        console.log(error.description)
+        return null
+      }
+    )
+
+    return answer
+  },
+
+  /**
+   *
+   * @param {*} id
+   * @returns The result of the database query
+   */
+  async getUser(id) {
+    if (typeof(id) != "number") {
+      console.log(typeof(id))
+      return null
+    }
+
+    const newRequest = new Request(baseURL + "/" + id, {
+      method: "GET",
+      headers: headers,
+      credentials: "omit"
+    });
+
+    /**
+     * @type {number}
+     */
+    const answer = await fetch(newRequest)
+    .then(
+      async (response) => {
+        const answer = await response.json()
+        .then(
+          /**
+           *
+           * @param {*} answer Response in json form
+           * @returns The user id if a valid statusCode is given. Negative one if otherwise
+           */
+          (answer) => {
+            if (answer.statusCode == 200) {
+              return {
+                statusCode: answer.statusCode,
+                user: answer.data.user
+              }
+            } else {
+              return {
+                statusCode: answer.statusCode,
+                error: answer.message
+              }
+            }
+          }
+        ).catch(
+          (error) => {
+            return {
+              statusCode: error.statusCode,
+              error: answer.message
+            }
+          }
+        )
         return answer
       }
     ).catch(
--- vueapp/src/views/UserView.vue
+++ vueapp/src/views/UserView.vue
@@ -4,13 +4,13 @@
   import { useRoute } from 'vue-router'
 
   // JavaScript files
-  import UserServiceDB from '../services/UserServiceDB.js'
   import UserService from '../services/UserService.js'
   import { CurrentUser } from '../state/CurrentUser.js'
   import { CurrentRoute } from '../state/CurrentRoute'
 
   const users = ref(null)
   const selectUser = ref(null)
+  const connected = ref(false)
 
   onMounted(async () => {
     // Change the current route name
@@ -18,29 +18,35 @@
 
     // UserService (fetchAPI)
     users.value = await UserService.getUsers()
+    if (users.value) {
+      connected.value = true
+    }
   })
 
   async function swapUsers() {
-    console.log(selectUser.value)
-    UserServiceDB.getUser(selectUser.value).then(
-      (response) => {
-        const user = response.data.data.user;
-        CurrentUser.changeUser(user.id)
-      }
-    ).catch(
-      (error) => {
-        console.log(error.message)
-      }
-    )
+
+    const response = await UserService.getUser(selectUser.value)
+
+    if (response.statusCode == 200) {
+      const user = response.user
+      CurrentUser.changeUser(user.id)
+    } else {
+      console.log(response.statusCode)
+      console.log(response.message)
+    }
   }
 </script>
 
 <template>
   <h1>User Database</h1>
-  <div class="users">
+  <div v-if="connected" class="users">
+    <h2>Select a Username</h2>
     <select name="User" id="UserList" v-model="selectUser" v-on:change="swapUsers()">
       <option v-for="user in users" :key="user.id" :value="user.id">{{ user.username }}</option>
     </select>
+  </div>
+  <div v-else class="error">
+    <p>ERROR: Database Connection failed</p>
   </div>
 </template>
 
